c214abc386bbd66ef44db27976efe430eb285f05
tschm <thomas.schmelzer@gmail.com>
2013-10-14 12:03:37 +0200

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='MosekRegression',
+    version='1.0',
+    packages=['util', 'mosekTools'],
+    url='https://github.com/tschm/MosekRegression',
+    license='',
+    author='Thomas Schmelzer, Joachim Dahl',
+    author_email='thomas.schmelzer@gmail.com',
+    description='Experiments & Tools for new Mosek Fusion interface'
+)
