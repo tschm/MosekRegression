f75744cf51cf1d75655225560f66b8f3fcd7b8a0
tschm <thomas.schmelzer@gmail.com>
2013-11-03 20:31:14 +0100

    doc

--- docs/make.bat
+++ docs/make.bat
@@ -115,9 +115,9 @@ if "%1" == "qthelp" (
 	echo.
 	echo.Build finished; now you can run "qcollectiongenerator" with the ^
 .qhcp project file in %BUILDDIR%/qthelp, like this:
-	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\mosekTools.qhcp
+	echo.^> qcollectiongenerator %BUILDDIR%\qthelp\MosekTools.qhcp
 	echo.To view the help file:
-	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\mosekTools.ghc
+	echo.^> assistant -collectionFile %BUILDDIR%\qthelp\MosekTools.ghc
 	goto end
 )
 
