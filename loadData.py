bee6b65bf055149f079e01a54bf60952438034d3
tschm <thomas.schmelzer@gmail.com>
2013-12-13 15:54:33 +0000

    fetching data

--- loadData.py
+++ loadData.py
@@ -1,9 +1,9 @@
 import datetime as dt
-from mosekTools.data.yahoo import get_data_yahoo
+import mosekTools.data.Data as Data
 
 if __name__ == '__main__':
     # fetch individual stocks and the S&P index
     s = dt.datetime(2010, 1, 1)
     e = dt.datetime(2012, 12, 31)
     symbols = ["GS", "AAPL", "IBM", "GOOG", "T", "^GSPC"]
-    get_data_yahoo(symbols=symbols, start=s, end=e).to_csv("data/data.csv")
+    Data.get_data_yahoo(symbols=symbols, start=s, end=e).to_csv("data/data.csv")
