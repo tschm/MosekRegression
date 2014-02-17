6681b2c5d225e3875a31b57f56cff26735ccc922
Thomas Schmelzer <thomas.schmelzer@lobnek.com>
2014-02-17 14:36:57 +0100

    enforcing int rather than long

--- mosekTools/solver/model/model.py
+++ mosekTools/solver/model/model.py
@@ -6,7 +6,7 @@ def weights(model, name, n, lb=-np.infty, ub=np.infty):
     """
     Introduce n positive weights for the portfolio
     """
-    return model.variable(name, n, Domain.inRange(lb, ub))
+    return model.variable(name, int(n), Domain.inRange(lb, ub))
 
 
 def build_model(name):
