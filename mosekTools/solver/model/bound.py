d9e32e9c3587c3e6aaa2ffd0d89b76335b62c08b
Thomas Schmelzer <thomas.schmelzer@lobnek.com>
2014-02-28 11:56:57 +0100

    bound with comments

--- mosekTools/solver/model/bound.py
+++ mosekTools/solver/model/bound.py
@@ -2,16 +2,28 @@ from mosek.fusion import *
 
 
 def upper(model, expr, bound):
+    """
+    upper bound for an expression in a model
+    """
     model.constraint(expr, Domain.lessThan(bound))
 
 
 def lower(model, expr, bound):
+    """
+    lower bound for an expression in a model
+    """
     model.constraint(expr, Domain.greaterThan(bound))
 
 
 def range(model, expr, lower, upper):
+    """
+    range bound for an expression in a model
+    """
     model.constraint(expr, Domain.inRange(lower, upper))
 
 
 def equal(model, expr, bound):
+    """
+    assigned value for an expression in a model
+    """
     model.constraint(expr, Domain.equalsTo(bound))
