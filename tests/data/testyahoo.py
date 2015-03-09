27d45ecd695a03fd8a1dec383cb77365e49fded9
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2015-03-09 15:21:02 +0100

    testData loading

--- tests/data/testyahoo.py
+++ tests/data/testyahoo.py
@@ -1,19 +1,19 @@
-import datetime as dt
-from nose.tools import raises, assert_almost_equal
+from unittest import TestCase
+from datetime import datetime
+from nose.tools import raises
 
 from mosekTools.data.Data import get_data_yahoo
 
-
-s = dt.datetime(2012, 1, 1)
-e = dt.datetime(2012, 12, 31)
-
-
-@raises(IOError)
-def test_load_wrong_symbol():
-    get_data_yahoo(symbols=["SYMBOL UNKNOWN"], start=s, end=e)
+s = datetime(2012, 1, 1)
+e = datetime(2012, 12, 31)
 
 
-def test_load_correct_symbol():
-    op = get_data_yahoo(symbols=["T"], start=s, end=e, series="Open")
-    assert_almost_equal(op.values[-1], 33.13, delta=1e-10)
-    assert_almost_equal(op.values[0], 30.46, delta=1e-10)
+class TestData(TestCase):
+    @raises(IOError)
+    def test_load_wrong_symbol(self):
+        get_data_yahoo(symbols=["SYMBOL UNKNOWN"], start=s, end=e)
+
+    def test_load_correct_symbol(self):
+        op = get_data_yahoo(symbols=["T"], start=s, end=e, series="Open")
+        self.assertAlmostEqual(op.values[-1], 33.13, delta=1e-10)
+        self.assertAlmostEqual(op.values[0], 30.46, delta=1e-10)
