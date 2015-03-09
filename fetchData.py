681f1cd139fdabd19076e18187197c79c5238fe4
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2015-03-09 16:03:57 +0100

    new data

--- fetchData.py
+++
@@ -1,6 +0,0 @@
-import datetime as dt
-import mosekTools.data.Data as Data
-
-### DAX ###
-symbols = Data.get_index_components("dax").index
-Data.get_data_yahoo(symbols=symbols, start=dt.datetime(2000, 7, 1)).to_csv("data/dax/data.csv")
