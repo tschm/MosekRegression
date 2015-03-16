213b1afd917d9afdf824594e6e6329b5aae85bc3
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2015-03-16 13:46:36 +0100

    remove obsolete Data package

--- loadData.py
+++ loadData.py
@@ -1,9 +1,10 @@
 from datetime import datetime
-import mosekTools.data.Data as Data
+import pandas.io.data as pw
 
 if __name__ == '__main__':
     # fetch individual stocks and the S&P index
     s = datetime(2010, 1, 1)
     e = datetime(2015, 3, 1)
     symbols = ["GS", "AAPL", "IBM", "GOOG", "T", "^GSPC"]
-    Data.get_data_yahoo(symbols=symbols, start=s, end=e).to_csv("data/data.csv")
+
+    pw.get_data_yahoo(symbols, s, e)["Adj Close"].to_csv("data/data.csv")
