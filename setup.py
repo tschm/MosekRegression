19836193c8446fdc67b2a6340c69e3ad439a6112
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2016-10-19 14:12:08 +0200

    setup

--- setup.py
+++ setup.py
@@ -1,22 +1,15 @@
-import sys
-import os
+#!/usr/bin/env python
+
 from setuptools import setup, find_packages
-
-version = "v1.1"
-
-if sys.argv[-1] == 'tag':
-    os.system("git tag -a %s -m 'version %s'" % (version, version))
-    os.system("git push --tags")
-    sys.exit()
 
 setup(
     name='MosekRegression',
-    version=version[1:],
+    version="v1.2",
     packages=find_packages(include='mosekTools*'),
     url='https://github.com/tschm/MosekRegression',
     license='',
     author='Thomas Schmelzer, Joachim Dahl',
     author_email='thomas.schmelzer@gmail.com',
     description='Experiments & Tools for the new Mosek Fusion interface',
-    install_requires=['pandas>=0.16.2', 'Mosek>=7.1']
+    install_requires=['pandas>=0.18.0', 'Mosek>=8.0']
 )
