c3c5871caaada47fdb2465b7e9ee08991b9052d2
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2023-04-28 16:39:12 -0700

    remove create kernel

--- book.sh
+++ book.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-pip install jupyter-book
+NAME="mosek"
+
+python -m venv .venv
+source .venv/bin/activate
+pip install -r requirements.txt
+pip install jupyter jupyter-book ipykernel
+
+ipython kernel install --name $NAME --user
+
 jupyter-book clean book
 jupyter-book build book
-cp -r /book/_build/html mosekbook
-
 
