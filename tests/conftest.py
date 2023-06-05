f032f9cf51bb9e6e5de3e26495d5e72b24a31cbc
Thomas Schmelzer <thomas.schmelzer@adia.ae>
2023-06-05 07:34:33 -0700

    conftest

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from pathlib import Path
+
+import pytest
+
+
+@pytest.fixture(scope="session")
+def resource_dir():
+    return Path(__file__).parent / "resources"
