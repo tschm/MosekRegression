4121133821777e5abef05ad88859fb04056d0507
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-01-04 08:43:52 +0400

    bandit?

--- tests/test_tools.py
+++ tests/test_tools.py
@@ -1,5 +1,11 @@
 from __future__ import annotations
 
+import pandas as pd
 
-def test_trivial():
-    assert True
+
+def test_min_variance(resource_dir):
+    data = pd.read_csv(resource_dir / "data.csv", index_col=0, parse_dates=True, header=0)
+    rets = data.pct_change()
+    cov = rets.cov()
+    print(cov)
+    # can't really test Mosek based fcts without the license :-)
