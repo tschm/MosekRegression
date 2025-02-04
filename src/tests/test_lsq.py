7b53de227e0934bb4026f55074e678a9b8d16f31
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-02-04 17:12:49 +0400

    tests

--- src/tests/test_lsq.py
+++ src/tests/test_lsq.py
@@ -1,6 +1,8 @@
 import pandas as pd
+import pytest
 
 from mosek_tools.solver import lsq_pos as ll
+from tests.utils.ci import is_ci_environment
 
 
 def lsq_pos(matrix, rhs):
@@ -11,6 +13,7 @@ def Sharpe_Ratio(ts):
     return 16 * ts.mean() / ts.std()
 
 
+@pytest.mark.skipif(is_ci_environment(), reason="Test requires local Mosek license")
 def test_lsq(resource_dir):
     data = pd.read_csv(resource_dir / "data.csv", index_col=0, parse_dates=True).ffill()
     returns = data.pct_change().fillna(0.0)
---
+++ src/tests/utils/ci.py
@@ -0,0 +1,19 @@
+import os
+
+
+def is_ci_environment():
+    """
+    Check if the code is running in a CI environment.
+    This uses common CI environment variables.
+    """
+    ci_vars = [
+        "CI",  # Generic CI flag
+        "GITHUB_ACTIONS",  # GitHub Actions
+        "TRAVIS",  # Travis CI
+        "CIRCLECI",  # CircleCI
+        "GITLAB_CI",  # GitLab CI
+        "JENKINS_HOME",  # Jenkins
+        "TEAMCITY_VERSION",  # TeamCity
+        "CODEBUILD_BUILD_ID",  # AWS CodeBuild
+    ]
+    return any(var in os.environ for var in ci_vars)
