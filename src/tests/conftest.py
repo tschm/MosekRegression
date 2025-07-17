b2729ec3b66fe6443771805179d224291bf248f1
Thomas Schmelzer <thomas.schmelzer@gmail.com>
2025-07-17 18:31:29 +0200

    test env with fixtures

--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -8,28 +8,3 @@ import pytest
 @pytest.fixture(scope="session")
 def resource_dir():
     return Path(__file__).parent / "resources"
-
-
-@pytest.fixture
-def project_root():
-    """Fixture that provides the project root directory.
-
-    Returns:
-        Path: The path to the project root directory.
-    """
-    return Path(__file__).parent.parent.parent
-
-
-@pytest.fixture
-def env_content(project_root: Path):
-    """Fixture that provides the content of the .env file as a dictionary.
-
-    Returns:
-        dict: A dictionary containing the key-value pairs from the .env file.
-    """
-    # Get the project root directory
-    env_file_path = project_root / ".env"
-
-    from dotenv import dotenv_values
-
-    return dotenv_values(env_file_path)
